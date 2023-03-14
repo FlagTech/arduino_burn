aa4afb30696c9263c57548b81f9673abce67be1b
meebox <meebox@gmail.com>
2023-03-14 14:17:16 +0800

    加入 FM615A 韌體與重複燒錄功能

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 65001
 if "%1"=="" goto usage
 if "%2"=="" goto usage
 if not exist %2 (
@@ -6,9 +7,16 @@ if not exist %2 (
   goto :end
 )
 
+:next
 .\avrdude.exe "-C.\avrdude.conf"  -v -V -patmega328p -carduino "-P%1" -b57600 -D "-Uflash:w:%2:i"
-goto end
+set /p dummy="燒錄完成, 請換下一片後按 Enter 繼續... (輸入 q 離開)"
+)
 
+if "%dummy%"=="q" (
+    goto end
+) else (
+    goto next
+)
 :usage
 echo 用法
 echo    run 序列埠  韌體檔路徑
